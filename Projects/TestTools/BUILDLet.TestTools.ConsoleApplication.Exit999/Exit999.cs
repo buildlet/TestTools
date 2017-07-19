31f9e486fa9dc66810185db9057259c3475c48ef
Daiki Sakamoto <dsakam@live.jp>
2017-07-19 23:24:54 +0900

    Version 2.0.1.0

---
+++ Projects/TestTools/BUILDLet.TestTools.ConsoleApplication.Exit999/Exit999.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUILDLet.TestTools.ConsoleApplication.Exit999
+{
+    class Exit999
+    {
+        static int Main(string[] args)
+        {
+            return 0x999;
+        }
+    }
+}
